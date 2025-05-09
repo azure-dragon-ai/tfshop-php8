a6e0651395de9d54bdc5d3074ac2c7fecd4607aa
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-05-09 18:11:43 +0800

    update

--- api/config/passport.php
+++ api/config/passport.php
@@ -1,15 +1,76 @@
 <?php
-/** +----------------------------------------------------------------------
- * | TFSHOP [ 轻量级易扩展低代码开源商城系统 ]
- * +----------------------------------------------------------------------
- * | Copyright (c) 2020~2023 https://www.dswjcms.com All rights reserved.
- * +----------------------------------------------------------------------
- * | Licensed 未经许可不能去掉TFSHOP相关版权
- * +----------------------------------------------------------------------
- * | Author: Purl <383354826@qq.com>
- * +----------------------------------------------------------------------
- */
+
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Passport Guard
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which authentication guard Passport will use when
+    | authenticating users. This value should correspond with one of your
+    | guards that is already present in your "auth" configuration file.
+    |
+    */
+
+    'guard' => 'web',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Encryption Keys
+    |--------------------------------------------------------------------------
+    |
+    | Passport uses encryption keys while generating secure access tokens for
+    | your application. By default, the keys are stored as local files but
+    | can be set via environment variables when that is more convenient.
+    |
+    */
+
+    'private_key' => env('PASSPORT_PRIVATE_KEY'),
+
+    'public_key' => env('PASSPORT_PUBLIC_KEY'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Passport Database Connection
+    |--------------------------------------------------------------------------
+    |
+    | By default, Passport's models will utilize your application's default
+    | database connection. If you wish to use a different connection you
+    | may specify the configured name of the database connection here.
+    |
+    */
+
+    'connection' => env('PASSPORT_CONNECTION'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Client UUIDs
+    |--------------------------------------------------------------------------
+    |
+    | By default, Passport uses auto-incrementing primary keys when assigning
+    | IDs to clients. However, if Passport is installed using the provided
+    | --uuids switch, this will be set to "true" and UUIDs will be used.
+    |
+    */
+
+    'client_uuids' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Personal Access Client
+    |--------------------------------------------------------------------------
+    |
+    | If you enable client hashing, you should set the personal access client
+    | ID and unhashed secret within your environment file. The values will
+    | get used while issuing fresh personal access tokens to your users.
+    |
+    */
+
+    'personal_access_client' => [
+        'id' => env('PASSPORT_PERSONAL_ACCESS_CLIENT_ID'),
+        'secret' => env('PASSPORT_PERSONAL_ACCESS_CLIENT_SECRET'),
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -50,4 +111,5 @@ return [
     'hash_client_secrets' => env('HASH_CLIENT_SECRETS', false),   //客户端秘钥的 hash 加密
     'expires_in' => env('PASSPORT_EXPIRES_IN','7200'),  // token失效时间(单位秒)，默认为2小时;这里设置超过1天，移动端的token刷新才会有效
     'refresh_expires_in' => env('REFRESH_PASSPORT_EXPIRES_IN','604800'),  // token刷新失效时间(单位秒)，默认为7天
+
 ];
