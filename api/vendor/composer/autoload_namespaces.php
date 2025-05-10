34f66ab5ebef5d01a827971457bb7190f36c43aa
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-05-10 11:51:39 +0800

    update

--- api/vendor/composer/autoload_namespaces.php
+++ api/vendor/composer/autoload_namespaces.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Pimple' => array($vendorDir . '/pimple/pimple/src'),
     'Parsedown' => array($vendorDir . '/erusev/parsedown'),
     'Mpociot' => array($vendorDir . '/mpociot/reflection-docblock/src'),
     'HTMLPurifier' => array($vendorDir . '/ezyang/htmlpurifier/library'),
